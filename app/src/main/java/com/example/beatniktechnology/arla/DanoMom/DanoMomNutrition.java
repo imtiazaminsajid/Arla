cf0e2489e167d8b8f0e00f00667d956ac52e5011
imtiazaminsajid <imtiazaminsajid@gmail.com>
2019-07-21 18:18:15 +0600

    Arla All Done Before Feedback

---
+++ app/src/main/java/com/example/beatniktechnology/arla/DanoMom/DanoMomNutrition.java
@@ -0,0 +1,47 @@
+package com.example.beatniktechnology.arla.DanoMom;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.beatniktechnology.arla.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DanoMomNutrition extends Fragment {
+
+    static DanoMomNutrition instance;
+
+
+    public DanoMomNutrition() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dano_mom_nutrition, container, false);
+    }
+
+    public static DanoMomNutrition getInstance() {
+        if (instance == null)
+            instance = new DanoMomNutrition();
+        return instance;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            // Refresh your fragment here
+            getFragmentManager().beginTransaction().detach(this).attach(this).commit();
+        }
+    }
+
+}
